7dde11f357279509828f783f04e8028b6e06ba63
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-25 14:35:35 -0300

    Add helmet middleware

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvService } from './env/env.service';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   const envService = app.get(EnvService);
   const port = envService.get('PORT');
